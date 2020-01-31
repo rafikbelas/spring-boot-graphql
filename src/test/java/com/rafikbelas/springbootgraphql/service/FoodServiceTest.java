e59f3cc7f36e24317f0f0991277049e6ec9eafa3
Rafik BELAS <rafik.belas@hotmail.fr>
2020-01-31 23:55:00 +0100

    Added test for getFoods endpoint.

---
+++ src/test/java/com/rafikbelas/springbootgraphql/service/FoodServiceTest.java
@@ -0,0 +1,40 @@
+package com.rafikbelas.springbootgraphql.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.htmlunit.MockMvcWebClientBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class FoodServiceTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Test
+    void getFoods() throws Exception {
+        String expectedResponse = "{\"data\":{\"foods\":[" +
+                "{\"id\":1,\"name\":\"Pizza\",\"isGood\":true}," +
+                "{\"id\":2,\"name\":\"Spam\",\"isGood\":false}," +
+                "{\"id\":3,\"name\":\"Eggs\",\"isGood\":true}," +
+                "{\"id\":4,\"name\":\"Avocado\",\"isGood\":false}" +
+                "]}}";
+
+        mockMvc.perform(MockMvcRequestBuilders.post("/graphql")
+                .content("{\"query\":\"{foods { id name isGood } }\"}")
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().json(expectedResponse))
+                .andReturn();
+    }
+}
